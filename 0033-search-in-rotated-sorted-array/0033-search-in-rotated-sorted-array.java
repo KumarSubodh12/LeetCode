caf4d12437a53d5876b37507ddc6d64f11ab0e3e
KumarSubodh12 <githubsubodh1212@gmail.com>
2026-05-22 10:12:36 +0530

    Time: 0 ms (100%), Space: 43.6 MB (80.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,27 @@
-
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            if (nums[mid] > nums[n - 1]) lo = mid + 1;
+            else hi = mid;
+        }
 
-            if (nums[mid] == target) return mid;
+        int rot = lo;
+        lo = 0; hi = n - 1;
 
-            // Check if left part is sorted
-            if (nums[mid] >= nums[start]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1; // move left
-                } else {
-                    start = mid + 1; // move right
-                }
-            } 
-            // Right part is sorted
-            else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1; // move right
-                } else {
-                    end = mid - 1; // move left
-                }
-            }
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            int real = (mid + rot) % n;
+
+            if (nums[real] == target)
+                return real;
+            
+            if (nums[real] < target) lo = mid + 1;
+            else hi = mid - 1;
         }
 
         return -1;
